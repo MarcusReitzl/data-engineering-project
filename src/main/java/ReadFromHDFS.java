beee5cd1bad291af1073e7f044f5c6edfb20ef23
MarcusReitzl <mareitzl@edu.aau.at>
2021-02-12 10:37:55 -0800

    restored Images from Bytes received from HDFS

--- src/main/java/ReadFromHDFS.java
+++ src/main/java/ReadFromHDFS.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,6 +8,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.hadoop.conf.Configuration;
@@ -20,6 +22,7 @@ import org.apache.hadoop.io.BytesWritable;
 
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 
 
 public class ReadFromHDFS {
@@ -38,7 +41,16 @@ public class ReadFromHDFS {
             BytesWritable key = new BytesWritable();
 
             while(reader.next(key, value)){
-                System.out.println("key: "+key+"value: "+value);
+                System.out.println("key: "+key+" value: "+value);
+                byte [] imageAsByteArray = Arrays.copyOf(key.getBytes(), key.getLength());
+                BufferedImage restoredImage = ImageIO.read(new ByteArrayInputStream(imageAsByteArray));
+
+                /*
+                Call if u want to see the restored images from hdfs but VM will die
+                 */
+                //showImage(restoredImage);
+
+
             }
             //in = filesystem.open(path);
             //IOUtils.copyBytes(in, System.out, 4096, false);
@@ -47,5 +59,13 @@ public class ReadFromHDFS {
         } finally {
             reader.close();
         }
+    }
+
+    private static void showImage(BufferedImage restoredImage) {
+        JFrame frame = new JFrame();
+        frame.setSize(300, 300);
+        JLabel label = new JLabel(new ImageIcon(restoredImage));
+        frame.add(label);
+        frame.setVisible(true);
     }
 }
